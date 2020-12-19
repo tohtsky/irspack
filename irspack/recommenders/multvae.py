2c1fd745829797ceef25da6f2073884613950299
Tomoki Ohtsuki <tomoki.ohtsuki129@gmail.com>
2020-12-19 01:52:18 +0000

    jax version works?

--- irspack/recommenders/multvae.py
+++ irspack/recommenders/multvae.py
@@ -1,5 +1,8 @@
-import warnings
-from typing import IO, List, Optional
+import pickle
+from dataclasses import dataclass
+from typing import IO, List, Optional, Any, Tuple
+import optax
+from optax import OptState, adam
 
 import numpy as np
 from scipy import sparse as sps
@@ -13,132 +16,113 @@ from ..definitions import (
 from .base_earlystop import BaseRecommenderWithEarlyStopping, TrainerBase
 from ..parameter_tuning import CategoricalSuggestion
 
-try:
-    import torch
-    from torch import nn
-    from torch.nn import functional as F
-except ModuleNotFoundError:
-    warnings.warn(
-        "failed to import torch. If you wish to use MultVAE, you have to install torch."
-    )
-    raise
+import haiku as hk
+import jax.numpy as jnp
+import jax
+
+PRNGKey = jax.random.PRNGKey
 
 
 class BaseMLP:
-    ACTIVATOR_CLASS = torch.nn.Tanh
-    classname = "BaseMLP"
-    regularize = False
-
     def __init__(
         self,
-        input_dim: int,
         output_dim: int,
-        hidden_dims: List[int],
-        dropout: Optional[float] = None,
+        hidden_dim: int,
     ):
+        self.output_dim = output_dim
+        self.hidden_dim = hidden_dim
 
-        super().__init__()
-        input_dims = [input_dim] + hidden_dims
-        output_dims = hidden_dims + [output_dim]
-        self.lt_names: List[str] = []
-        self.n_layers = len(input_dims)
-        self.dropout: Optional[nn.Dropout]
-        self.activator = self.ACTIVATOR_CLASS()
-        if dropout is not None:
-            self.dropout = nn.Dropout(dropout)
-        else:
-            self.dropout = None
-
-        for i, (d_in, d_out) in enumerate(zip(input_dims, output_dims)):
-            name = f"{self.classname}_layer_{i}"
-
-            setattr(self, name, nn.Linear(d_in, d_out))
-            self.lt_names.append(name)
-
-    def _lts(self) -> List[nn.Linear]:
-        return [getattr(self, name) for name in self.lt_names]
-
-    def weights_sq_sum(self) -> List[torch.Tensor]:
-        return [(lt.weight ** 2).sum() for lt in self._lts()]
-
-    def forward(self, h: torch.Tensor) -> torch.Tensor:
-        if self.dropout is not None:
-            h = self.dropout(h)
-        for i, lt in enumerate(self._lts()):
-            h = lt(h)
-            if i < (self.n_layers - 1):
-                h = self.activator(h)
-        return h
+    def __call__(
+        self,
+        X: jnp.ndarray,
+    ) -> jnp.ndarray:
+        mlp = hk.Sequential(
+            [hk.Linear(self.hidden_dim), jnp.tanh, hk.Linear(self.output_dim)]
+        )
+        return mlp(X)
 
 
-class DecoderNN(BaseMLP, nn.Module):
-    def __init__(self, latent_dim: int, output_dim: int, hidden_dims: int):
-        super().__init__(latent_dim, output_dim, hidden_dims)
+class DecoderNN:
+    def __init__(self, output_dim: int, hidden_dim: int):
+        mlp = BaseMLP(output_dim=output_dim, hidden_dim=hidden_dim)
+        self.mlp = mlp
 
-    def forward(self, z):
-        h = super().forward(z)
-        return F.log_softmax(h, dim=1)
-
-
-def l2_normalize(X):
-    return X / torch.sqrt((X ** 2).sum(dim=1) + 1e-8).unsqueeze(1)
+    def __call__(self, X: jnp.ndarray) -> jnp.ndarray:
+        return jax.nn.log_softmax(self.mlp(X), axis=1)
 
 
-class EncoderNN(BaseMLP, nn.Module):
-    def __init__(self, input_dim, latent_dim, hidden_dims, dropout=0.5):
+def l2_normalize(X: jnp.ndarray) -> jnp.ndarray:
+    return X / jnp.sqrt((X ** 2).sum(axis=1) + 1e-8)[:, None]
+
+
+class EncoderNN:
+    def __init__(
+        self,
+        latent_dim: int,
+        hidden_dim: int,
+    ):
+        self.mlp = BaseMLP(2 * latent_dim, hidden_dim)
         self.latent_dim = latent_dim
-        super().__init__(input_dim, 2 * latent_dim, hidden_dims, dropout=dropout)
 
-    def forward(self, X):
+    def __call__(
+        self, X: jnp.ndarray, dropout: float, train: bool
+    ) -> Tuple[jnp.ndarray, jnp.ndarray]:
         X = l2_normalize(X)
-        h = super().forward(X)
+        if train:
+            X = hk.dropout(hk.next_rng_key(), dropout, X)
+        h = self.mlp(X)
         mu = h[:, : self.latent_dim]
         log_var = h[:, self.latent_dim :]
         return mu, log_var
 
 
-class MultVAE(torch.nn.Module):
+@dataclass
+class MultVAEOutput:
+    log_softmax: jnp.ndarray
+    mean: jnp.ndarray
+    log_stddev: jnp.ndarray
+    KL: jnp.ndarray
+
+
+class MultVAE:
     def __init__(
         self,
         n_obs: int,
         latent_dim: int,
-        enc_hidden_dims: List[int],
-        dec_hidden_dims: List[int],
+        enc_hidden_dim: int,
+        dec_hidden_dim: int,
         dropout_p: float = 0.5,
         l2_reg: float = 0.01,
     ):
-        super().__init__()
-        self.q_network = EncoderNN(n_obs, latent_dim, enc_hidden_dims, dropout=0.5)
-        self.p_network = DecoderNN(latent_dim, n_obs, dec_hidden_dims)
-        self._kl_coeff = 0.0
+        self.encoder_network = EncoderNN(
+            latent_dim,
+            enc_hidden_dim,
+        )
+        self.decoder_network = DecoderNN(n_obs, dec_hidden_dim)
+        self._kl_coeff: float = 0.0
         self.dropout_p = dropout_p
         self.l2_reg = l2_reg
 
-    def set_kl_coeff(self, nv):
+    def set_kl_coeff(self, nv: float) -> None:
         self._kl_coeff = nv
 
-    def kl_coeff(self):
+    def kl_coeff(self) -> float:
         return self._kl_coeff
 
-    def forward(self, X):
-
-        mu, log_var = self.q_network(X)
-        std = torch.exp(log_var * 0.5)
-
+    def __call__(
+        self, X: jnp.ndarray, p: jnp.ndarray, train: bool
+    ) -> MultVAEOutput:
+        mu, log_var = self.encoder_network(X, p, train)
+        std = jnp.exp(log_var * 0.5)
         # log_var = 2 * log (std)
-        KL = (0.5 * (-log_var + std ** 2 + mu ** 2 - 1)).sum(dim=1).mean(dim=0)
+        KL: jnp.ndarray = 0.5 * (-log_var + std ** 2 + mu ** 2 - 1)
+        KL = KL.sum(axis=1).mean(axis=0)
 
-        eps = torch.randn(mu.shape)
-        z = mu + eps * std
-        log_softmax = self.p_network(z)
+        eps = jax.random.normal(hk.next_rng_key(), mu.shape)  # self.rng.rand
+        z: jnp.ndarray = mu + eps * std
+        log_softmax: jnp.ndarray = self.decoder_network(z)
 
-        q_weights = sum(self.q_network.weights_sq_sum())
-        p_weights = sum(self.p_network.weights_sq_sum())
-        neg_ll = -(log_softmax * X).sum(dim=1).mean(dim=0)
-        neg_elbo = neg_ll + self._kl_coeff * KL
-        if self.l2_reg > 0:
-            neg_elbo = neg_elbo + (q_weights + p_weights) * self.l2_reg
-        return neg_elbo
+        return MultVAEOutput(log_softmax, mu, log_var * 0.5, KL)
 
 
 class MultVAETrainer(TrainerBase):
@@ -146,13 +130,14 @@ class MultVAETrainer(TrainerBase):
         self,
         X: InteractionMatrix,
         dim_z: int,
-        enc_hidden_dims: List[int],
-        dec_hidden_dims: Optional[List[int]],
+        enc_hidden_dim: int,
+        dec_hidden_dim: Optional[int],
         dropout_p: float,
         l2_regularizer: float,
         kl_anneal_goal: float,
         anneal_end_epoch: int,
         minibatch_size: int,
+        learning_rate: float,
     ):
         self.X = X
         self.n_user = X.shape[0]
@@ -160,72 +145,122 @@ class MultVAETrainer(TrainerBase):
         self.minibatch_size = minibatch_size
         self.kl_anneal_goal = kl_anneal_goal
         self.anneal_end_epoch = anneal_end_epoch
-        if dec_hidden_dims is None:
-            dec_hidden_dims = enc_hidden_dims
-        self.vae = MultVAE(
-            self.n_item,
-            dim_z,
-            enc_hidden_dims,
-            dec_hidden_dims,
-            dropout_p=dropout_p,
-            l2_reg=l2_regularizer,
-        )
-        self.total_anneal_step = (anneal_end_epoch * self.n_user) / minibatch_size
-        self.recommend_with_randomness = False
+        self.dropout_p = dropout_p
 
-        self.optimizer = torch.optim.Adam(self.vae.parameters())
+        self.rng_seq = hk.PRNGSequence(42)
+
+        if dec_hidden_dim is None:
+            dec_hidden_dim = enc_hidden_dim
+
+        n_item = self.n_item
+        vae_f = hk.transform(
+            lambda X, p, train: MultVAE(
+                n_item,
+                dim_z,
+                enc_hidden_dim,
+                dec_hidden_dim,
+                dropout_p=dropout_p,
+                l2_reg=l2_regularizer,
+            )(X, p, train)
+        )
+
+        optimizer = adam(learning_rate=learning_rate)
+        params = vae_f.init(
+            next(self.rng_seq),
+            np.zeros((1, X.shape[1]), dtype=np.float32),
+            self.dropout_p,
+            True,
+        )
+        self.opt_state = optimizer.init(params)
+        self.total_anneal_step = (
+            anneal_end_epoch * self.n_user
+        ) / minibatch_size
+        self.recommend_with_randomness = False
         self._update_count = 0
+
+        def loss_fn(
+            params: hk.Params,
+            rng: jnp.ndarray,
+            X: jnp.ndarray,
+            kl_coeff: jnp.ndarray,
+            dropout: float,
+            train: bool,
+        ) -> jnp.ndarray:
+            mvresult: MultVAEOutput = vae_f.apply(
+                params, rng, X, dropout, train
+            )
+            neg_ll = -(mvresult.log_softmax * X).sum(axis=1).mean()
+            neg_elbo = neg_ll + kl_coeff * mvresult.KL
+            return neg_elbo
+
+        loss_fn = jax.jit(loss_fn, static_argnums=(4, 5))
+
+        def update(
+            params: hk.Params,
+            rng: jnp.ndarray,
+            opt_state: OptState,
+            X: jnp.ndarray,
+            kl_coeff: jnp.ndarray,
+            dropout: float,
+        ) -> Tuple[hk.Params, OptState]:
+            grads = jax.grad(loss_fn)(params, rng, X, kl_coeff, dropout, True)
+            updates, new_optstate = optimizer.update(grads, opt_state)
+            new_params = optax.apply_updates(
+                params,
+                updates,
+            )
+            return new_params, new_optstate
+
+        update = jax.jit(update, static_argnums=(5,))
+        self.opt_state = self.opt_state
+        self.update_function = (update,)
+        self.vae_f = vae_f
+        self.params = params
 
     def get_score_cold_user(self, X: InteractionMatrix) -> DenseScoreArray:
         mb_arrays: List[DenseScoreArray] = []
         n_users: int = X.shape[0]
-        self.vae.eval()
         X_csr: sps.csr_matrix = X.tocsr()
-        with torch.no_grad():
-            for mb_start in range(0, n_users, self.minibatch_size):
-                mb_end = min(n_users, mb_start + self.minibatch_size)
-                X_mb = X_csr[mb_start:mb_end].astype(np.float32).toarray()
-                X_torch = torch.tensor(X_mb, requires_grad=False)
-                mu, log_var = self.vae.q_network(X_torch)
-                if self.recommend_with_randomness:
-                    z = mu + torch.exp(log_var * 0.5) * torch.randn(mu.shape)
-                else:
-                    z = mu
-                mb_arrays.append(self.vae.p_network(z).detach().numpy())
-        score_concat = np.concatenate(mb_arrays, axis=0)
-        return score_concat.astype(np.float64)
+        for mb_start in range(0, n_users, self.minibatch_size):
+            mb_end = min(n_users, mb_start + self.minibatch_size)
+            X_mb = X_csr[mb_start:mb_end].astype(np.float32).toarray()
+            X_jax = jnp.asarray(X_mb)
+            mvresult: MultVAEOutput = self.vae_f.apply(
+                self.params, next(self.rng_seq), X_jax, self.dropout_p, False
+            )
 
-    def run_epoch(self, **kwargs) -> None:
-        self.vae.train()
+            mb_arrays.append(np.asarray(mvresult.log_softmax, dtype=np.float64))
+        score_concat = np.concatenate(mb_arrays, axis=0)
+        return score_concat
+
+    def run_epoch(self) -> None:
         user_indices = np.arange(self.n_user)
         np.random.shuffle(user_indices)
-        mean_loss = []
         for mb_start in range(0, self.n_user, self.minibatch_size):
-            current_kl_coeff = self.kl_anneal_goal * min(
-                1, self._update_count / self.total_anneal_step
+            current_kl_coeff = jnp.asarray(
+                self.kl_anneal_goal
+                * min(1, self._update_count / self.total_anneal_step)
             )
-            self.vae.set_kl_coeff(current_kl_coeff)
-
             mb_end = min(self.n_user, mb_start + self.minibatch_size)
             X = self.X[user_indices[mb_start:mb_end]].astype(np.float32)
             if sps.issparse(X):
                 X = X.toarray()
-            X_torch = torch.tensor(X, requires_grad=False)
-            loss = self.vae(X_torch)
-            self.optimizer.zero_grad()
-            mean_loss.append(loss.detach().numpy())
-            loss.backward()
-            self.optimizer.step()
+            X_jax = jnp.asarray(X, dtype=jnp.float32)
+            self.params, self.opt_state = self.update_function[0](
+                self.params,
+                next(self.rng_seq),
+                self.opt_state,
+                X_jax,
+                current_kl_coeff,
+                self.dropout_p,
+            )
             self._update_count += 1
 
-        loss = np.asarray(mean_loss).mean()
-
     def save_state(self, ofs: IO) -> None:
-        torch.save(self.vae.state_dict(), ofs)
+        pickle.dump(self.params, ofs)
 
     def load_state(self, ifs: IO) -> None:
-        self.vae.load_state_dict(torch.load(ifs))
-        self.vae.eval()
+        self.params = pickle.load(ifs)
 
 
 class MultVAERecommender(
@@ -241,8 +276,8 @@ class MultVAERecommender(
         self,
         X_all: InteractionMatrix,
         dim_z: int = 16,
-        enc_hidden_dims: List[int] = [256],
-        dec_hidden_dims: Optional[List[int]] = None,
+        enc_hidden_dims: int = 256,
+        dec_hidden_dims: Optional[int] = None,
         dropout_p: float = 0.5,
         l2_regularizer: float = 0,
         kl_anneal_goal: float = 0.2,
@@ -251,7 +286,7 @@ class MultVAERecommender(
         max_epoch: int = 300,
         validate_epoch: int = 5,
         score_degradation_max: int = 5,
-        recommend_with_randomness=False,
+        learning_rate: float = 1e-3,
     ) -> None:
         super().__init__(
             X_all,
@@ -266,9 +301,9 @@ class MultVAERecommender(
         self.kl_anneal_goal = kl_anneal_goal
         self.anneal_end_epoch = anneal_end_epoch
         self.minibatch_size = minibatch_size
-        self.recommend_with_randomness = recommend_with_randomness
         self.dropout_p = dropout_p
         self.l2_regularizer = l2_regularizer
+        self.learning_rate = learning_rate
 
         self.trainer: Optional[MultVAETrainer] = None
 
@@ -283,6 +318,7 @@ class MultVAERecommender(
             self.kl_anneal_goal,
             self.anneal_end_epoch,
             self.minibatch_size,
+            self.learning_rate,
         )
 
     def get_score(self, user_indices: UserIndexArray) -> DenseScoreArray:
