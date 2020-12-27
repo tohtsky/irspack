e871dab65cf8dc265907a8d4c5e708e7b9105740
Tomoki Ohtsuki <tomoki.ohtsuki129@gmail.com>
2020-12-27 00:18:43 +0000

    update pyi

--- irspack/utils/_util_cpp.pyi
+++ irspack/utils/_util_cpp.pyi
@@ -1,57 +1,31 @@
+from numpy import float32
+import irspack.utils._util_cpp
+from typing import *
 from typing import Iterable as iterable
 from typing import Iterator as iterator
-from typing import *
-
-from numpy import float32, float64
-
-import irspack.utils._util_cpp
-
+from numpy import float64
 _Shape = Tuple[int, ...]
 import scipy.sparse
-
-__all__ = [
-    "okapi_BM_25_weight",
-    "remove_diagonal",
-    "rowwise_train_test_split_d",
-    "rowwise_train_test_split_f",
-    "rowwise_train_test_split_i",
-    "sparse_mm_threaded",
-    "tf_idf_weight",
+__all__  = [
+"okapi_BM_25_weight",
+"remove_diagonal",
+"rowwise_train_test_split_d",
+"rowwise_train_test_split_f",
+"rowwise_train_test_split_i",
+"sparse_mm_threaded",
+"tf_idf_weight"
 ]
-
-def okapi_BM_25_weight(
-    X: scipy.sparse.csr_matrix[float64], k1: float = 1.2, b: float = 0.75
-) -> scipy.sparse.csr_matrix[float64]:
+def okapi_BM_25_weight(X: scipy.sparse.csr_matrix[float64], k1: float = 1.2, b: float = 0.75) -> scipy.sparse.csr_matrix[float64]:
     pass
-
-def remove_diagonal(
-    arg0: scipy.sparse.csr_matrix[float64],
-) -> scipy.sparse.csr_matrix[float64]:
+def remove_diagonal(arg0: scipy.sparse.csr_matrix[float64]) -> scipy.sparse.csr_matrix[float64]:
     pass
-
-def rowwise_train_test_split_d(
-    arg0: scipy.sparse.csr_matrix[float64], arg1: float, arg2: int
-) -> Tuple[scipy.sparse.csc_matrix[float64], scipy.sparse.csr_matrix[float64]]:
+def rowwise_train_test_split_d(arg0: scipy.sparse.csr_matrix[float64], arg1: float, arg2: int) -> Tuple[scipy.sparse.csc_matrix[float64], scipy.sparse.csr_matrix[float64]]:
     pass
-
-def rowwise_train_test_split_f(
-    arg0: scipy.sparse.csr_matrix[float32], arg1: float, arg2: int
-) -> Tuple[scipy.sparse.csc_matrix[float32], scipy.sparse.csr_matrix[float32]]:
+def rowwise_train_test_split_f(arg0: scipy.sparse.csr_matrix[float32], arg1: float, arg2: int) -> Tuple[scipy.sparse.csc_matrix[float32], scipy.sparse.csr_matrix[float32]]:
     pass
-
-def rowwise_train_test_split_i(
-    arg0: scipy.sparse.csr_matrix[float32], arg1: float, arg2: int
-) -> Tuple[scipy.sparse.csc_matrix[float32], scipy.sparse.csr_matrix[float32]]:
+def rowwise_train_test_split_i(arg0: scipy.sparse.csr_matrix[float32], arg1: float, arg2: int) -> Tuple[scipy.sparse.csc_matrix[float32], scipy.sparse.csr_matrix[float32]]:
     pass
-
-def sparse_mm_threaded(
-    arg0: scipy.sparse.csr_matrix[float64],
-    arg1: scipy.sparse.csc_matrix[float64],
-    arg2: int,
-) -> scipy.sparse.csr_matrix[float64]:
+def sparse_mm_threaded(arg0: scipy.sparse.csr_matrix[float64], arg1: scipy.sparse.csc_matrix[float64], arg2: int) -> scipy.sparse.csr_matrix[float64]:
     pass
-
-def tf_idf_weight(
-    X: scipy.sparse.csr_matrix[float64], smooth: bool = True
-) -> scipy.sparse.csr_matrix[float64]:
+def tf_idf_weight(X: scipy.sparse.csr_matrix[float64], smooth: bool = True) -> scipy.sparse.csr_matrix[float64]:
     pass
