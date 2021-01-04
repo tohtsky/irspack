1cc9a65767b92a8bf23c6aa6d45801a2ab689576
Tomoki Ohtsuki <tomoki.ohtsuki129@gmail.com>
2021-01-04 14:26:07 +0000

    add a function to hold-out specific interactions

--- irspack/split/__init__.py
+++ irspack/split/__init__.py
@@ -1,8 +1,10 @@
 from ..utils import rowwise_train_test_split
 from .random import UserLearnPredictPair, split_dataframe_partial_user_holdout
+from .specified import holdout_specific_interactions
 
 __all__ = [
     "UserLearnPredictPair",
     "split_dataframe_partial_user_holdout",
+    "holdout_specific_interactions",
     "rowwise_train_test_split",
 ]
