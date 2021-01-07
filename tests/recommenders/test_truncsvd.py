3091b23682fb99ebf8ed90c1bb5264a71f596e3d
Tomoki Ohtsuki <tomoki.ohtsuki129@gmail.com>
2021-01-07 04:11:21 +0000

    Added a detailed test for truncsvd.

---
+++ tests/recommenders/test_truncsvd.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pytest
+import scipy.sparse as sps
+
+from irspack.recommenders import TruncatedSVDRecommender
+
+RNS = np.random.RandomState(0)
+
+X = RNS.rand(200, 512)
+X[X <= 0.9] = 0
+X[X > 0.9] = 1
+X = sps.csr_matrix(X)
+
+
+def test_truncsvd() -> None:
+    with pytest.warns(UserWarning):
+        overfit_rec = TruncatedSVDRecommender(X, n_components=X.shape[1] + 1)
+
+    with pytest.raises(RuntimeError):
+        overfit_rec.get_score_block(0, X.shape[0])
+
+    with pytest.raises(RuntimeError):
+        overfit_rec.get_item_embedding()
+
+    with pytest.raises(RuntimeError):
+        overfit_rec.get_user_embedding()
+
+    overfit_rec.learn()
+    X_reproduced = overfit_rec.get_score_block(0, X.shape[0])
+    X_frobenius = (X.data ** 2).sum()
+    residual = (X_reproduced - X).A1
+    assert ((residual ** 2).sum() / X_frobenius) < 1e-3
