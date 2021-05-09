2cab6175d02a3445cfc078e2eb64d6b624264427
Tomoki Ohtsuki <tomoki.ohtsuki129@gmail.com>
2021-05-09 09:08:09 +0000

    More elaborate tests

--- tests/autopilot/test_autopilot.py
+++ tests/autopilot/test_autopilot.py
@@ -1,25 +1,21 @@
-import os
 import sys
 import time
-from typing import Dict, List
+from typing import Dict
 
 import numpy as np
 import pandas as pd
 import pytest
 from scipy import sparse as sps
 
-from irspack import (
-    BaseOptimizer,
-    BaseRecommender,
-    DenseScoreArray,
-    InteractionMatrix,
-    UserIndexArray,
-    autopilot,
-)
+from irspack import autopilot
 from irspack.evaluator.evaluator import Evaluator
-from irspack.parameter_tuning import Suggestion, UniformSuggestion
 
-SKIP_TEST = os.environ.get("IRSPACK_TESTING", "false") != "true"
+from .mock_classes import (
+    AutopilotMockEarlyStoppableRecommender,
+    AutopilotMockEvaluator,
+    AutopilotMockOptimizer,
+    AutopilotMockRecommender,
+)
 
 TIMESCALE = 1.5
 
@@ -31,57 +27,31 @@ X_answer = sps.csr_matrix(
 )
 
 
-class AutopilotMockRecommender(BaseRecommender):
-    def __init__(self, X: InteractionMatrix, wait_time: float = 1.0):
-        super().__init__(X)
-        self.wait_time = wait_time
-        self.leaked = X_answer.toarray()
-
-    def get_score(self, user_indices: UserIndexArray) -> DenseScoreArray:
-        return self.leaked[user_indices]
-
-    def _learn(self) -> None:
-        time.sleep(self.wait_time)
-
-
-class AutoPilotMockOptimizer(BaseOptimizer):
-    recommender_class = AutopilotMockRecommender
-    default_tune_range: List[Suggestion] = [UniformSuggestion("wait_time", 0.1, 4.0)]
-
-    @classmethod
-    def tune_range_given_memory_budget(
-        cls, X: InteractionMatrix, memory_in_mb: int
-    ) -> List[Suggestion]:
-        return []
-
-
-@pytest.mark.skipif(SKIP_TEST, reason="May not work on non-Linux.")
 def test_autopilot() -> None:
-    if sys.platform == "win32":
-        pytest.skip("Skip on Windows.")
+    # if sys.platform == "win32":
+    pytest.skip("Skip on Windows.")
 
-    evaluator = Evaluator(X_answer, 0)
+    evaluator = AutopilotMockEvaluator(X_answer)
     recommender_class, best_param, trial_df = autopilot(
         X_small,
         evaluator,
         memory_budget=1,
         n_trials=10,
-        algorithms=["AutoPilotMock"],
+        algorithms=["AutopilotMock"],
         timeout_singlestep=2,
     )
     assert best_param["wait_time"] < 2.0
     assert trial_df.shape[0] == 10
-    wait_times = trial_df["AutoPilotMockOptimizer.wait_time"]
+    wait_times = trial_df["AutopilotMockOptimizer.wait_time"]
     assert np.all(trial_df.iloc[(wait_times.values > 2.0)]["ndcg@10"].isna())
     assert recommender_class is AutopilotMockRecommender
 
 
-@pytest.mark.skipif(SKIP_TEST, reason="May not work on non-Linux.")
 def test_autopilot_timeout() -> None:
-    if sys.platform == "win32":
-        pytest.skip("Skip on Windows.")
+    # if sys.platform == "win32":
+    pytest.skip("Skip on Windows.")
 
-    evaluator = Evaluator(X_answer, 0)
+    evaluator = AutopilotMockEvaluator(X_answer)
     wait = 20
     with pytest.raises(RuntimeError):
         # no available algorithm
@@ -101,7 +71,7 @@ def test_autopilot_timeout() -> None:
 
     def callback(trial_number: int, history_df: pd.DataFrame) -> None:
         wait_times_given_by_callback[trial_number] = history_df[
-            "AutoPilotMockOptimizer.wait_time"
+            "AutopilotMockOptimizer.wait_time"
         ].iloc[-1]
 
     recommender_class, best_param, trial_df = autopilot(
@@ -109,23 +79,56 @@ def test_autopilot_timeout() -> None:
         evaluator,
         memory_budget=1,
         n_trials=100,
-        algorithms=["AutoPilotMock", "DenseSLIM"],
+        algorithms=["AutopilotMock", "DenseSLIM"],
         timeout_overall=wait,
         timeout_singlestep=1,
         callback=callback,
     )
     end = time.time()
     assert wait < (end - start) + 1
-    wait_times = trial_df["AutoPilotMockOptimizer.wait_time"]
+    wait_times = trial_df["AutopilotMockOptimizer.wait_time"]
     assert np.all(trial_df.iloc[(wait_times.values > 1.0)]["ndcg@10"].isna())
     # dense slim should be skipped
     assert len({name for name in trial_df["optimizer_name"] if not pd.isna(name)}) == 1
     assert recommender_class is AutopilotMockRecommender
     recommender_class(X_small, **best_param).learn()
     for index, row in trial_df.iterrows():
-        target_value = row["AutoPilotMockOptimizer.wait_time"]
+        target_value = row["AutopilotMockOptimizer.wait_time"]
         test_value = wait_times_given_by_callback[index]
         if pd.isna(test_value):
             assert pd.isna(target_value)
         else:
             assert target_value == test_value
+
+
+def test_autopilot_earlystop() -> None:
+    if sys.platform == "win32":
+        pytest.skip("Skip on Windows.")
+
+    evaluator = AutopilotMockEvaluator(X_answer)
+    recommender_class, best_param, trial_df = autopilot(
+        X_small,
+        evaluator,
+        memory_budget=1,
+        n_trials=20,
+        algorithms=["AutopilotMockEarlyStoppable"],
+        timeout_singlestep=2,
+        random_seed=0,
+    )
+    assert trial_df.shape[0] == 20
+    wait_times = trial_df["AutopilotMockEarlyStoppableOptimizer.wait_time"]
+    assert np.all(trial_df.iloc[(wait_times.values > 4e-1)]["ndcg@10"].isna())
+    no_result_location = np.where(
+        (5 * trial_df["AutopilotMockEarlyStoppableOptimizer.wait_time"]) > 2
+    )[0]
+    assert no_result_location.shape[0] > 0  # asserting this test is meaningful
+    assert np.all(trial_df.iloc[no_result_location].value == 0)
+
+    result_location = np.where(
+        (5 * trial_df["AutopilotMockEarlyStoppableOptimizer.wait_time"])
+        < 1.0  # They should reach at least epoch = 5
+    )[0]
+    assert result_location.shape[0] > 0  # asserting this test is meaningful
+    assert np.all(trial_df.iloc[result_location].value < 0)
+
+    assert recommender_class is AutopilotMockEarlyStoppableRecommender
